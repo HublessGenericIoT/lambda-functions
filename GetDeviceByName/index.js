4745eafd1f7a3700cd627381273bce010dea229e
David Tschida <dmtschida1@gmail.com>
2016-03-04 12:59:32 -0500

    GetDevicesByName gets with the shadow

--- GetDeviceByName/index.js
+++ GetDeviceByName/index.js
@@ -2,22 +2,35 @@ console.log('Loading function');
 
 var aws = require('aws-sdk');
 var iot = new aws.Iot();
-
+var iotData = new aws.IotData({endpoint : "A2SJ249R53BDX8.iot.us-east-1.amazonaws.com"});
 exports.handler = function(event, context) {
 
-    if(!event.name) {
-        context.fail("Please provide a device name in the path.")
-    }
-    var params = {
-        thingName: event.name /* required */
-    };
-    iot.describeThing(params, function(err, data) {
-        if (err) {
-            console.log("Error: " + err, err.stack); // an error occurred
-            context.fail("An error occured querying from IOT. Please reformat your request");
-            return;
-        }
-        context.succeed(data);
-        return;
-    });
+	if(!event.name) {
+		context.fail("Please provide a device name in the path.")
+	}
+	var params = {
+		thingName: event.name /* required */
+	};
+	iot.describeThing(params, function(err, thing) {
+		if (err) {
+			console.log("Error: " + err, err.stack); // an error occurred
+			context.fail("An error occured querying from IOT. Please reformat your request");
+			return;
+		}
+
+		iotData.getThingShadow(params, function(err, data) {
+			var shadow = {};
+			console.log("Data from the iotData sdk", data);
+			if (err) {
+				console.log("An error occurred in the iotData api", err);
+				if(err.statusCode == 404) {
+					shadow = {};
+				}
+				shadow = {};
+			} else {
+				shadow = JSON.parse(data.payload);
+			}
+			context.succeed({status: "Success", payload: {thing: thing, shadow: shadow}});
+		});
+	});
 };
