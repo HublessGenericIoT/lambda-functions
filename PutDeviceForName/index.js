83789d79f53b971738ba93da18b52a256b175daf
David Tschida <dmtschida1@gmail.com>
2016-03-10 19:08:16 -0500

    Fixed Put

--- PutDeviceForName/index.js
+++ PutDeviceForName/index.js
@@ -4,26 +4,32 @@ var aws = require('aws-sdk');
 var iot = new aws.Iot();
 
 exports.handler = function(event, context) {
-
     if(!event.name) {
         context.fail("Please provide a device name in the path.");
-    }
-    if(Object.keys(event.attributes).length > 3) {
+		return;
+	}
+	if(!event.device.attributes) {
+		context.fail("Please include an attributes key in your body.");
+		return;
+	}
+    if(Object.keys(event.device.attributes).length > 3) {
         context.fail("A thing can only have three keys. Please try again")
+		return;
     }
     var params = {
       attributePayload: { /* required */
-        attributes: event.attributes
+        attributes: event.device.attributes
       },
       thingName: event.name /* required */
     };
     iot.updateThing(params, function(err, data) {
         if (err) {
             console.log("Error: " + err, err.stack); // an error occurred
-            context.fail("An error occured querying from IOT. Please reformat your request");
+            context.fail("An error occurred querying from IOT. Please reformat your request");
             return;
         }
-        context.succeed({status: "Success"});
+		console.log("Payload : ", data);
+        context.succeed({status: "Success", payload: data});
         return;
     });
 };
